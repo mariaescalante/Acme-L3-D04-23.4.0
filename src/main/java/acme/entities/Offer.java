e39d849d9a33c122665e19ccb51f6b0487974035
Daniel Diañez <danidiasua02@gmail.com>
2023-03-02 11:38:49 +0100

    Offer update

--- src/main/java/acme/entities/Offer.java
+++ src/main/java/acme/entities/Offer.java
@@ -8,11 +8,11 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
-import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -44,8 +44,8 @@ public class Offer extends AbstractEntity {
 	@Temporal(TemporalType.TIMESTAMP)
 	protected Date				availabilityPeriod;
 
-	@PositiveOrZero
-	protected Double			price;
+	//FALTA POR HACER UNA VALIDATION
+	protected Money				price;
 
 	@URL
 	protected String			optionalLink;
