b45c22d78d8d328eacfb7dda85b95ec92c183aa3
mariaescalante <marescram3@alum.us.es>
2023-03-04 17:44:03 +0100

    Entidad Enrolment creada

---
+++ src/main/java/acme/entities/Enrolment.java
@@ -0,0 +1,43 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import acme.roles.Student;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Enrolment extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			motivation;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+	protected Integer			workTime;
+
+	@OneToOne(mappedBy = "enrolment")
+	protected Workbook			workbook;
+
+	@ManyToOne()
+	protected Student			student;
+
+}
