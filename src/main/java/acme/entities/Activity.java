eb3603ba120bf497bd75e2ca7002d41bbe137776
mariaescalante <marescram3@alum.us.es>
2023-03-04 17:44:53 +0100

    Entidad Activity creada

---
+++ src/main/java/acme/entities/Activity.java
@@ -0,0 +1,39 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Activity extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			summary;
+
+	protected ActivityType		type;
+
+	protected Double			duration;
+
+	@URL
+	protected String			link;
+
+	@ManyToOne()
+	protected Workbook			workbook;
+}
