e6709b2657faf27b48a7691ea67ada5fc509aba7
mariaescalante <marescram3@alum.us.es>
2023-03-04 17:41:02 +0100

    Entidad Student completada

--- src/main/java/acme/roles/Student.java
+++ src/main/java/acme/roles/Student.java
@@ -7,13 +7,14 @@ import javax.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.data.AbstractRole;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Student {
+public class Student extends AbstractRole {
 
 	protected static final long	serialVersionUID	= 1L;
 
