2014b0ea22da2266aa38c277ed234118899a45cd
fracamcam <fracamcam@alum.us.es>
2023-03-09 13:20:10 +0100

    Small fix in Peep entity

--- src/main/java/acme/entities/Peep.java
+++ src/main/java/acme/entities/Peep.java
@@ -4,8 +4,11 @@ package acme.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 
 import org.hibernate.validator.constraints.Length;
@@ -22,7 +25,9 @@ public class Peep extends AbstractEntity {
 
 	protected static final long	serialVersionUID	= 1L;
 
+	@Temporal(TemporalType.TIMESTAMP)
 	@PastOrPresent
+	@NotNull
 	protected Date				instantiationMoment;
 
 	@NotBlank
