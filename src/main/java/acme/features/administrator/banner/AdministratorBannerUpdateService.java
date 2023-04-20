a1a692196aec75e34967f77c1ca45873feaf26ff
Joaquín Arregui Díaz <joaquin.arregui2002@gmail.com>
2023-04-20 09:36:05 +0200

    Completed requirement

---
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerUpdateService.java
@@ -0,0 +1,93 @@
+
+package acme.features.administrator.banner;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Banner;
+import acme.framework.components.accounts.Administrator;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AdministratorBannerUpdateService extends AbstractService<Administrator, Banner> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AdministratorBannerRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		boolean status;
+		status = super.getRequest().hasData("id", int.class);
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Banner object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneBannerById(id);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Banner object) {
+		assert object != null;
+
+		super.bind(object, "start", "end", "picture", "slogan", "url");
+	}
+
+	@Override
+	public void validate(final Banner object) {
+		assert object != null;
+		Date date;
+
+		if (!super.getBuffer().getErrors().hasErrors("end"))
+			super.state(object.getStart().before(object.getEnd()), "end", "company.practicum.form.error.start-before-end");
+
+		if (!super.getBuffer().getErrors().hasErrors("start"))
+			super.state(object.getStart().after(object.getInstantiationMoment()), "start", "company.practicum.form.error.start-future");
+
+		if (!super.getBuffer().getErrors().hasErrors("end")) {
+			date = AdministratorBannerCreateService.sumarDiasAFecha(object.getInstantiationMoment(), 7);
+			super.state(object.getEnd().after(date) || object.getStart().equals(date), "end", "company.practicum.form.error.least-one-week-long");
+		}
+	}
+
+	@Override
+	public void perform(final Banner object) {
+		assert object != null;
+
+		object.setInstantiationMoment(Date.from(Instant.now().minus(1, ChronoUnit.MINUTES)));
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Banner object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "instantiationMoment", "start", "end", "picture", "slogan", "url");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/administrator/banner/list.jsp
@@ -0,0 +1,11 @@
+<%@page language="java"%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="urn:jsptagdir:/WEB-INF/tags"%>
+
+<acme:list>
+	<acme:list-column code="administrator.banner.list.label.slogan" path="slogan"  width="50%"/>
+	<acme:list-column code="administrator.banner.list.label.start" path="start" width="25%" />
+	<acme:list-column code="administrator.banner.list.label.end" path="end" width="25%" />
+</acme:list>
+<acme:button code="administrator.banner.list.button.create" action="/administrator/banner/create"/>
