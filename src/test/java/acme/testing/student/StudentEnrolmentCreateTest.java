00abd6bca423fb0867c97dc2e791748d3eab51b6
mariaescalante <marescram3@alum.us.es>
2023-05-11 10:44:40 +0200

    StudentEnrolmentCreateTest

---
+++ src/test/java/acme/testing/student/StudentEnrolmentCreateTest.java
@@ -0,0 +1,108 @@
+
+package acme.testing.student;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class StudentEnrolmentCreateTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/student/enrolment/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String code, final String motivation, final String goals, final String workbook, final String course, final String creditCard) {
+		// HINT: this test authenticates as a student and then lists his or her enrolments, creates a new one, and check that it's been created properly.
+
+		super.signIn("student1", "student1");
+
+		super.clickOnMenu("Student", "My enrolments");
+		super.checkListingExists();
+
+		super.clickOnButton("Create");
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("motivation", motivation);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("workbook", workbook);
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("creditCard", creditCard);
+		super.clickOnSubmit("Create");
+
+		super.clickOnMenu("Student", "My enrolments");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, workbook);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("motivation", motivation);
+		super.checkInputBoxHasValue("goals", goals);
+		super.checkInputBoxHasValue("workbook", workbook);
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("creditCard", creditCard);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/student/enrolment/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int recordIndex, final String code, final String motivation, final String goals, final String workbook, final String course, final String creditCard) {
+		// HINT: this test attempts to create enrolments with incorrect data.
+
+		super.signIn("student1", "student1");
+
+		super.clickOnMenu("Student", "My enrolments");
+		super.checkListingExists();
+
+		super.clickOnButton("Create");
+		super.checkFormExists();
+
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("motivation", motivation);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("workbook", workbook);
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("creditCard", creditCard);
+		super.clickOnSubmit("Create");
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to create an enrolment using principals with inappropriate roles.
+
+		super.checkLinkExists("Sign in");
+		super.request("/student/enrolment/create");
+		super.checkPanicExists();
+
+		super.signIn("administrator", "administrator");
+		super.request("/student/enrolment/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("assistant1", "assistant1");
+		super.request("/student/enrolment/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("lecturer1", "lecturer1");
+		super.request("/student/enrolment/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("auditor1", "auditor1");
+		super.request("/student/enrolment/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("company1", "company1");
+		super.request("/student/enrolment/create");
+		super.checkPanicExists();
+		super.signOut();
+	}
+}
