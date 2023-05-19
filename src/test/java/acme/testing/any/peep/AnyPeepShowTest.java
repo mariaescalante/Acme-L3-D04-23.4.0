5e5ab9e0a45962173d35ca34930cd9120797ae9e
mariaescalante <marescram3@alum.us.es>
2023-05-19 12:40:32 +0200

    AnyPeepShowTest

--- src/test/java/acme/testing/any/peep/AnyPeepShowTest.java
+++ src/test/java/acme/testing/any/peep/AnyPeepShowTest.java
@@ -1,6 +1,32 @@
+
 package acme.testing.any.peep;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AnyPeepShowTest {
+import acme.testing.TestHarness;
+
+public class AnyPeepShowTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/peep/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String instantiationMoment, final String title, final String nickname, final String message, final String email, final String url) {
+
+		super.signIn("student1", "student1");
+
+		super.clickOnMenu("Any", "List All peeps");
+		super.sortListing(1, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("instantiationMoment", instantiationMoment);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("nickname", nickname);
+		super.checkInputBoxHasValue("message", message);
+		super.checkInputBoxHasValue("email", email);
+		super.checkInputBoxHasValue("url", url);
+
+		super.signOut();
+	}
 
 }
