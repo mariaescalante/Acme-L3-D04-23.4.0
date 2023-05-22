1ba4ab2494aacaed1d92eb4a4f58e74e7adb85b3
mariaescalante <marescram3@alum.us.es>
2023-05-22 19:34:28 +0200

    AssistantTutorialPublishTest y AssistantTutorialShowTest

---
+++ src/test/java/acme/testing/assistant/AssistantTutorialShowTest.java
@@ -0,0 +1,92 @@
+
+package acme.testing.assistant;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.Tutorial;
+import acme.testing.TestHarness;
+
+public class AssistantTutorialShowTest extends TestHarness {
+
+	@Autowired
+	protected AssistantTutorialTestRepository repository;
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String code, final String title, final String abstract$, final String goal, final String course) {
+
+		super.signIn("assistant1", "assistant1");
+
+		super.clickOnMenu("Assistant", "My tutorials");
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstract$", abstract$);
+		super.checkInputBoxHasValue("goal", goal);
+		super.checkInputBoxHasValue("course", course);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test200Negative() {
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to show an unpublished tutorial by someone who is not the principal.
+
+		Collection<Tutorial> tutorials;
+		String param;
+
+		tutorials = this.repository.findManyTutorialsByAssistantUsername("assistant1");
+		for (final Tutorial tutorial : tutorials)
+			if (tutorial.isDraftMode()) {
+				param = String.format("id=%d", tutorial.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+
+				super.signIn("administrator", "administrator");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("assistant2", "assistant2");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("auditor1", "auditor1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("student1", "student1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("lecturer1", "lecturer1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("company1", "company1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+			}
+
+	}
+}
